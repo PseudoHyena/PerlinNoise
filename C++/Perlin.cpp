0c1ce632f6ca95f28371c419ee98131c9c174aa8
Artem Bagnyuk <anzen.bagnyuk@gmail.com>
2017-11-20 11:14:17 +0300

    Add C++ prj

---
+++ C++/Perlin.cpp
@@ -0,0 +1,27 @@
+#include "Perlin.h"
+
+float Perlin::Noise2D(float x, float y){
+	return 0;
+}
+
+float Perlin::Lerp(float node0, float node1, float weight){
+	return ((float)1.0 - weight) * node0 + weight * node1;
+}
+
+float Perlin::DotGridGradient(int floor_x, int floor_y, float x, float y){
+	//grad vect
+
+	float distance_x = x - (float)floor_x;
+	float distance_y = y - (float)floor_y;
+
+	return 0;
+}
+
+Perlin::Perlin()
+{
+}
+
+
+Perlin::~Perlin()
+{
+}
