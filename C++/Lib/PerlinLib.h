c6a310542c3d7dd4ac9657bc091be711abaa9d31
Artem Bagnyuk <anzen.bagnyuk@gmail.com>
2017-12-06 18:35:42 +0300

    Add completed version of lib

---
+++ C++/Lib/PerlinLib.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace NoiseGeneration {
+
+	class Perlin {
+		static unsigned char _defaultPermutationTable[256];
+
+		unsigned char _permutationTable[512];
+
+		double Fade(double t);
+		double GradientDotProduction(int hash, double x, double y);
+		double GradientDotProduction(int hash, double x);
+		double Lerp(double a, double b, double x);
+
+	public:
+		double Noise(double x);
+		double Noise(double x, double y);
+
+		Perlin();
+	};
+}
